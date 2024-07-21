034629d6a3c3b9e81f79771d1f702b58ae17d2fb
cmathur13 <cmathur@seattleu.edu>
2024-07-21 10:01:41 -0400

    final update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <nav className="mb-20 flex item-center justify-between py-6">
       <div className="flex flex-shrink-0 items-center">
         {/* <img src={logo} alt="Logo" className="mx-2 w-10" /> */}
-        <span className="text-4xl font-kaushan">Ask</span>
+        <span className="text-4xl font-kaushan">CM</span>
       </div>
       <div className="m-8 flex item-center justify-center gap-4 text-2xl">
         {/* Remove links from here, and add light dark mode   */}
